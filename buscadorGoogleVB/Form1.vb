b38e2cd4aaf23ceb2d14c407e234a3202e702dab
jpromanonet <programming@jpromano.net>
2020-04-06 22:06:41 -0300

    Buscador en VB - Clase 02

---
+++ buscadorGoogleVB/Form1.vb
@@ -0,0 +1,5 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        System.Diagnostics.Process.Start("https://www.google.com/search?sxsrf=ALeKk03KIdJ0PFhqnFEsf3keRtMERvT02A%3A1586220695851&source=hp&ei=l86LXrLWMYbD5OUPp_SV0A8&q=" + TextBox1.Text + "&oq=" + TextBox1.Text + "&gs_lcp=CgZwc3ktYWIQAzICCAAyAggAMgIIADICCAAyBwgAEAoQywEyAggAMgQIABAKMgQIABAKMgIIADIFCAAQywE6BQgAEIMBShgIFxIUMGc5NWc5M2c5N2c4NGcxMTFnODVKEQgYEg0wZzFnMWcxZzFnMWcxULkNWOgVYPgWaAFwAHgAgAFZiAGwA5IBATaYAQCgAQGqAQdnd3Mtd2l6&sclient=psy-ab&ved=0ahUKEwjy_e_IjNXoAhWGIbkGHSd6BfoQ4dUDCAY&uact=5")
+    End Sub
+End Class
