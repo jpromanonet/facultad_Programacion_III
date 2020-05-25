024d23a0cbaa96737d326a4930be481a5d256033
jpromanonet <programming@jpromano.net>
2020-05-24 21:01:23 -0300

    Clase 08 - SQL

---
+++ Clase_08/vistaTotalFacturacion.sql
@@ -0,0 +1,20 @@
+USE Northwind
+
+GO
+
+CREATE VIEW totalFacturacion AS
+
+SELECT TOP (100) PERCENT dbo.Orders.OrderID AS NFact, 
+    SUM((dbo.[Order Details].UnitPrice * 21 / 100 + dbo.[Order Details].UnitPrice) * dbo.[Order Details].Quantity) AS Total
+        FROM dbo.Customers 
+            INNER JOIN dbo.Orders 
+                ON dbo.Customers.CustomerID = dbo.Orders.CustomerID 
+            INNER JOIN dbo.[Order Details] 
+                ON dbo.Orders.OrderID = dbo.[Order Details].OrderID 
+            INNER JOIN dbo.Products 
+                ON dbo.[Order Details].ProductID = dbo.Products.ProductID
+                    GROUP BY dbo.Orders.OrderID;
+
+SELECT * 
+    FROM FacturaTotal 
+        ORDER BY NFact
